7dd25bec5b13142ce7a5f982cea5609cb2323e06
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-06-13 22:49:59 +0300

    dark mode implementation

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
 
   return (
-    <div className="container">
+    <div className={`container ${theme}`}>
       <NavBar theme={theme} setTheme={setTheme} />
     </div>
   );
--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,31 +1,37 @@
-import '../Navbar/Navbar.css';
-import magnifyGlass from '../../assets/magnifyGlass.png';
-import sun from '../../assets/sun.png';
-import moon from '../../assets/moon.png';
-import darklogo from '../../assets/darklogo.jpg'
+import "../Navbar/Navbar.css";
+import magnifyGlass from "../../assets/magnifyGlass.png";
+import sun from "../../assets/sun.png";
+import moon from "../../assets/moon.png";
+import darklogo from "../../assets/darklogo.jpg";
+import whitelogo from '../../assets/whitelogo.jpg';
 
-const NavBar = () => {
+const NavBar = ({ theme, setTheme }) => {
+
+    const toggle_mode = () =>{
+        theme == 'light' ? setTheme('dark') : setTheme('light');
+    }
+
   return (
-    <div className='navbar'>
-        <img src={darklogo} alt="" className='logo' />
-        <ul>
-            <li>Cameras</li>
-            <li>Lenses & Filters</li>
-            <li>Accessories</li>
-            <li>Grip</li>
-            <li>Lighting</li>
-            <li>Sound</li>
-            <li>Contact Us</li>
-            <li>About Us</li>
-        </ul>
-        <div className='search-box'>
-            <input type='text' placeholder='Search' />
-            <img src={magnifyGlass} alt="" />
-        </div>
+    <div className={`navbar ${theme}`}>
+      <img src={theme == 'light' ? whitelogo : darklogo} alt="" className="logo" />
+      <ul>
+        <li>Cameras</li>
+        <li>Lenses & Filters</li>
+        <li>Accessories</li>
+        <li>Grip</li>
+        <li>Lighting</li>
+        <li>Sound</li>
+        <li>Contact Us</li>
+        <li>About Us</li>
+      </ul>
+      <div className={`search-box ${theme}`}>
+        <input type="text" placeholder="Search" />
+        <img src={magnifyGlass} alt="" />
+      </div>
 
-        <img src={moon} alt="" className='toggle-icon' />
+      <img onClick={() =>{toggle_mode()}} src={theme == 'light' ? moon : sun} alt="" className="toggle-icon" />
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
