e87d6af136c76d56da72267d2b67dcf9327c6216
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-06-17 15:52:30 +0300

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,35 +1,17 @@
 import './footer.css';
+import facebook from '../../assets/'
 
 const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer-container">
                 <div className="footer-column">
-                    <h4>About Us</h4>
-                    <p>We are a leading provider of camera and photography equipment, offering a wide range of products and services to help you capture the perfect shot.</p>
-                </div>
-                <div className="footer-column">
-                    <h4>Services</h4>
-                    <ul>
-                        <li>Cameras</li>
-                        <li>Lenses & Filters</li>
-                        <li>Accessories</li>
-                        <li>Grip</li>
-                        <li>Lighting</li>
-                        <li>Sound</li>
-                    </ul>
-                </div>
-                <div className="footer-column">
                     <h4>Contact Us</h4>
                     <ul>
-                        <li>Email: info@example.com</li>
-                        <li>Phone: (123) 456-7890</li>
-                        <li>Address: 1234 Street Name, City, State, 12345</li>
+                        <li>Email: office@magicshoprental.com</li>
+                        <li>Phone: +359 897 89 05 53</li>
+                        <li>Address: Levski – G 47, Sofia, Bulgaria</li>
                     </ul>
-                </div>
-                <div className="footer-column">
-                    <h4>Follow Us</h4>
-                    
                 </div>
             </div>
             <div className="footer-bottom">
