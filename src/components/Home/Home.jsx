27d5d668a4854c85a13c8f29120092082a8056aa
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-06-25 12:11:13 +0300

    css update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -6,10 +7,35 @@ import data from "../../data.js";
 import Card from "./Card";
 
 const Home = () => {
+  // Array of image URLs for the banner slideshow
+  const bannerImages = [
+    'image1.jpg',
+    'image2.jpg',
+    'image3.jpg',
+    // Add more image URLs as needed
+  ];
+
+  // Settings for the Slider
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+
   return (
     <>
       <div className="banner">
-
+        <Slider {...settings}>
+          {bannerImages.map((image, index) => (
+            <div key={index}>
+              <img src={image} alt={`Slide ${index + 1}`} style={{ width: '100%' }} />
+            </div>
+          ))}
+        </Slider>
       </div>
       <div>
         {data.map((item, index) => (
