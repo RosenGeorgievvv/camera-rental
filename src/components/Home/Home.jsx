a6b60bc6bbfc74ef73342dd16fa6702e42a7163b
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-06-25 12:02:35 +0300

    project banner

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,28 @@
-import data from '../../data.js';
-import Card from './Card';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+import data from "../../data.js";
+import Card from "./Card";
 
 const Home = () => {
   return (
-    <div>
-      {data.map((item, index) => (
-        <Card
-          key={index}
-          img={item.img}
-          name={item.name}
-          des={item.des}
-        />
-      ))}
-    </div>
+    <>
+      <div className="banner">
+
+      </div>
+      <div>
+        {data.map((item, index) => (
+          <Card
+            key={index}
+            img={item.img}
+            name={item.name}
+            des={item.des}
+            type={item.type}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/data.js
+++ src/data.js
@@ -1,19 +1,23 @@
-import sonyFX3 from '../src/assets/cameras/sony-fx3.jpeg'
+import sonyFX3 from '../src/assets/cameras/sony-fx3.jpeg';
+import sonyA7SIII from '../src/assets/cameras/sony-A7SIII.jpeg';
+import canon5d from '../src/assets/cameras/canon-5d-mark4.jpg';
 
 export default[
     {
         img: sonyFX3,
-        name: "Sony FX3",
-        type: 'photo'
+        name: "SONY FX3",
+        type: 'photo',
+        des: ""
+
     },
     {
-        img: cam2,
-        name: "Camera One",
-        des: "This is an example text. It will be replaced with actual description."
+        img: sonyA7SIII,
+        name: "SONY A7SIII",
+        des: ""
     },
     {
-        img: cam2,
-        name: "Camera One",
-        des: "This is an example text. It will be replaced with actual description."
+        img: canon5d,
+        name: "CANON 5d mark 4",
+        des: "30.4MP (CMOS Bayer RGB) / Full-Frame (36x24mm) / DCI 4K (4096 x 2160)30p / Canon EF / 1/8000s / 1/200s"
     }
 ]
