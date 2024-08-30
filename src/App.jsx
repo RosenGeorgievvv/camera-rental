7e4dc6caa1aa22b099e93d795f2f397239898269
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-08-30 17:32:47 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { useState } from "react";
-
 import './App.css';
 import NavBar from "./components/Navbar/NavBar";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Main/Home";
-import Contact from "./components/Contact/Contact";
-import Welcome from "./components/Main/Welcome";
+
 
 function App() {
   const [theme, setTheme] = useState("light");
@@ -15,7 +13,7 @@ function App() {
     <>
     <div className={`container ${theme}`}>
       <NavBar theme={theme} setTheme={setTheme} />
-      <Welcome />
+      <Home />
     </div>
     <Footer theme={theme} />
     
